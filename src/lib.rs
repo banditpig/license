4a48968051b8f083b640d545e55c45d73743bcb7
banditpig <mike_k_houghton@yahoo.co.uk>
2022-02-09 19:30:12 +0000

    Restructuring of License.

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,25 @@ mod license;
 
 #[cfg(test)]
 mod tests {
-    use chrono::NaiveDate;
-
     use crate::license::License;
 
     #[test]
-    fn build() {
-        let date_str = "2024-01-01";
-        let naive_date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap();
+    fn to_from_json() {
         let lic = License::new()
             .with_feature("debug".to_string(), "parts1".to_string())
             .with_feature("emails".to_string(), "email1, email2".to_string())
             .with_feature("admin".to_string(), "fred, joe".to_string())
             .with_feature("remote connect".to_string(), "yes".to_string())
-            .with_expiry(naive_date)
+            .with_expiry("2024-02-28")
             .with_max_users(10)
             .with_keyphrase("new license being made".to_string())
-            .sign()
-            .to_json();
+            .sign();
 
-        println!("{:?}", lic);
-        let lic2 = License::from_json(&lic);
-        println!("{:?}", lic2);
+        let lic_json = lic.all_to_json();
+
+        let lic2 = License::all_from_json(&lic_json);
+
+        println!("{:?}", lic2.all_to_json());
+        assert_eq!(lic, lic2);
     }
 }
--- src/license.rs
+++ src/license.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::panic::set_hook;
 
 use chrono::NaiveDate;
 use rand::rngs::OsRng;
@@ -8,66 +9,121 @@ use uuid::Uuid;
 
 //
 //
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde_with::serde_as]
-pub struct License {
-    id: String,
-    expires: NaiveDate,
-    features: HashMap<String, String>,
-    max_users: usize,
-    keyphrase: String,
+pub struct SigningData {
     sig_bytes: Vec<u8>,
+    pub_key: Vec<u8>,
 }
 
-impl License {
-    pub fn new() -> License {
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde_with::serde_as]
+pub struct UserData {
+    id: String,
+    pub expires: NaiveDate,
+    pub features: HashMap<String, String>,
+    pub max_users: usize,
+    keyphrase: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde_with::serde_as]
+pub struct License {
+    // id: String,
+    // pub expires: NaiveDate,
+    // pub features: HashMap<String, String>,
+    // pub max_users: usize,
+    // keyphrase: String,
+    user_data: UserData,
+    signing_data: SigningData,
+}
+
+impl SigningData {
+    pub fn new() -> Self {
+        Self {
+            sig_bytes: Vec::new(),
+            pub_key: Vec::new(),
+        }
+    }
+}
+
+impl UserData {
+    pub fn new() -> Self {
         let date_str = "2000-01-01";
         let naive_date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap();
         let guid = Uuid::new_v4();
-        License {
+        Self {
             id: guid.to_string(),
             expires: naive_date,
             features: HashMap::new(),
             max_users: 0,
             keyphrase: "".to_string(),
-            sig_bytes: Vec::new(),
         }
     }
-    pub fn from_json(json: &str) -> License {
-        let lic: License = serde_json::from_str(json).unwrap();
-        lic
+}
+
+impl License {
+    pub fn new() -> License {
+        // set_hook(Box::new(|info| {
+        //     println!("{}", info.to_string());
+        // }));
+        set_hook(Box::new(|info| {
+            println!("{:?}", info.to_string());
+        }));
+
+        License {
+            user_data: UserData::new(),
+            signing_data: SigningData::new(),
+        }
     }
+
     pub fn with_feature(mut self, key: String, val: String) -> License {
-        self.features.insert(key, val);
+        self.user_data.features.insert(key, val);
         self
     }
 
-    pub fn with_expiry(mut self, exp: NaiveDate) -> License {
-        self.expires = exp;
+    pub fn with_expiry(mut self, exp: &str) -> License {
+        let naive_date = NaiveDate::parse_from_str(exp, "%Y-%m-%d");
+        match naive_date {
+            Ok(d) => self.user_data.expires = d,
+            Err(_) => panic!("Error parsing date"),
+        }
+
         self
     }
     pub fn with_id(mut self, id: String) -> License {
-        self.id = id;
+        self.user_data.id = id;
         self
     }
     pub fn with_max_users(mut self, max_users: usize) -> License {
-        self.max_users = max_users;
+        self.user_data.max_users = max_users;
         self
     }
     pub fn with_keyphrase(mut self, keyphrase: String) -> License {
-        self.keyphrase = keyphrase;
+        self.user_data.keyphrase = keyphrase;
         self
     }
-    pub fn to_json(&self) -> String {
+    pub fn all_to_json(&self) -> String {
         serde_json::to_string(&self).unwrap()
+    }
+    pub fn user_data_to_json(&self) -> String {
+        serde_json::to_string(&self.user_data).unwrap()
+    }
+    pub fn user_data_from_json(json: &str) -> UserData {
+        serde_json::from_str(json).expect("Unable to make UserData from supplied json.")
+    }
+    pub fn all_from_json(json: &str) -> License {
+        serde_json::from_str(json).expect("Unable to make License from supplied json.")
     }
     pub fn sign(mut self) -> License {
         let keypair = Keypair::generate_with(OsRng);
-        let message = self.to_json();
-        let context = signing_context(self.keyphrase.as_bytes());
+
+        let message = self.user_data_to_json();
+        let context = signing_context(self.user_data.keyphrase.as_bytes());
         let signature = keypair.sign(context.bytes(message.as_bytes()));
 
-        self.sig_bytes = signature.to_bytes().to_vec();
+        self.signing_data.sig_bytes = signature.to_bytes().to_vec();
+        self.signing_data.pub_key = keypair.public.to_bytes().to_vec();
         self
     }
 }
